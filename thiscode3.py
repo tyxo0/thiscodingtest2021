3599c4fd5c1637ba52902c5b20540eb07d6ded72
roxnook233 <roxnook233@gmail.com>
2024-03-10 16:52:38 +0900

    Q3

--- thiscode3.py
+++ thiscode3.py
@@ -1,1 +1,15 @@
-print("Hello World!!") #
+#숫자 사이에 +, * 를 삽입하여 가장 큰 수 만들기
+inp = input()
+lis = list(map(int, inp)) # int형으로 저장
+#print(type(lis[1]))
+res = lis[0]
+for i in lis[1: ]:
+    # if res + i > res * i:
+    #     res += i
+    # else:
+    #     res *= i
+    if i < 2:
+        res += i
+    else:
+        res *= i 
+print(res)
